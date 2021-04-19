745ea29876148fab30fdd7e262b7ccc312abbb83
Jacques Peeters <jacques.peeters@manomano.com>
2021-04-19 18:35:08 +0200

    Change target again

--- preprocess.py
+++ preprocess.py
@@ -207,8 +207,8 @@ def generate_actuals(df_machine):
         # as otherwise it might be operating and performing within
         # the same minute, but not necessarily at the same time
         # see the drama here https://unearthed.solutions/u/competitions/96/forum#/question/897a06c1-34d1-4b1f-a016-464ee81111fc # noqa
-        # .assign(y_0=lambda _: _["operating"])  # & _["performing"])
-        .assign(y_0=lambda _: _["operating"] & _["performing"])
+        .assign(y_0=lambda _: _["operating"])  # & _["performing"])
+        # .assign(y_0=lambda _: _["operating"] & _["performing"])
         .groupby(["machine", "window"])[["operating", "performing", "y_0"]]
         .max()
         .assign(y_1=lambda _: _["y_0"].shift(periods=-1 * 60))
@@ -236,8 +236,8 @@ def _build_x_input(df_machine_tmp):
         # as otherwise it might be operating and performing within
         # the same minute, but not necessarily at the same time
         # see the drama here https://unearthed.solutions/u/competitions/96/forum#/question/897a06c1-34d1-4b1f-a016-464ee81111fc # noqa
-        # .assign(y_0=lambda _: _["operating"])  # & _["performing"])
-        .assign(y_0=lambda _: _["operating"] & _["performing"])
+        .assign(y_0=lambda _: _["operating"])  # & _["performing"])
+        # .assign(y_0=lambda _: _["operating"] & _["performing"])
         .groupby(["machine", "window"])[cols_target]
         .transform("max")
         .astype(int)
